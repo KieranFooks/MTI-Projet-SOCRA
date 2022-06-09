7bef01a24533a78dc575155065274969196cbe36
KieranFooks <kieranfooks@gmail.com>
2022-06-09 11:44:17 +0200

    refactor: Move server listen to server.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerFile from './swagger.json'
 
 const app = express()
-const PORT = process.env.PORT
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
@@ -11,6 +10,4 @@ app.get('/', (_req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(PORT, () => {
-  return console.log(`Express is listening at http://localhost:${PORT}`)
-})
+export default app
