ba4ef3f83d4ad76baa874506b2b4859cc0304ba4
Hugo Poujol <hugo.poujol@epita.fr>
2022-06-11 17:05:03 +0200

    test: Add get parcours with parameters integration tests

--- src/__test__/integration/Parcours.test.ts
+++ src/__test__/integration/Parcours.test.ts
@@ -19,7 +19,7 @@ const testModuleMTI2 : Module = new Module('S2', [new Subject('VueJS', 'Initiati
 const testModuleSRS1 : Module = new Module('S1', [new Subject('Forensic et reverse engineering', 'Initiation aux forensic et reverse engineering'), new Subject('Supervision de sécurité, ingénierie des SOC, détection opérationnelle', 'Initiation à la supervision de sécurité, ingénierie des SOC, détection opérationnelle')])
 const testModuleSRS2 : Module = new Module('S2', [new Subject('Stage en entreprise', 'Stage effectué en entreprise')])
 const testParcoursMTI : Parcours = new Parcours('MTI', 'Paris', 24, 'Master', 20000, 80, new Date(), [testModuleMTI1, testModuleMTI2], 'Super Majeur')
-const testParcoursSRS : Parcours = new Parcours('SRS', 'Paris', 24, 'Master', 10000, 100, new Date(), [testModuleSRS1, testModuleSRS2], 'Majeur bof bof')
+const testParcoursSRS : Parcours = new Parcours('SRS', 'Rennes', 24, 'Master', 10000, 100, new Date(), [testModuleSRS1, testModuleSRS2], 'Majeur bof bof')
 
 beforeAll(async () => {
   await AppDataSource.initialize()
@@ -61,4 +61,36 @@ test('GET /parcours should return the MTI and SRS parcours ordered by creation d
   expect(get.body.length).toBe(2)
   expect(get.body[0].title).toBe(testParcoursSRS.title)
   expect(get.body[1].title).toBe(testParcoursMTI.title)
+})
+
+test('GET /parcours with campus parameter set to Paris should return the MTI', async () => {
+  await AppDataSource.manager.save(testParcoursMTI)
+  await AppDataSource.manager.save(testParcoursSRS)
+
+  const get = await request.get('/parcours').query({campus: 'Paris'})
+
+  expect(get.statusCode).toBe(StatusCodes.OK)
+  expect(get.body.length).toBe(1)
+  expect(get.body[0].title).toBe(testParcoursMTI.title)
+})
+
+test('GET /parcours with campus parameter set to Paris and type paramter set to Licence should return 0 parcours', async () => {
+  await AppDataSource.manager.save(testParcoursMTI)
+  await AppDataSource.manager.save(testParcoursSRS)
+
+  const get = await request.get('/parcours').query({campus: 'Paris', type: 'Licence'})
+
+  expect(get.statusCode).toBe(StatusCodes.OK)
+  expect(get.body.length).toBe(0)
+})
+
+test('GET /parcours with campus parameter set to Rennes and type paramter set to Master and cost parameter set to 15000 should return the SRS', async () => {
+  await AppDataSource.manager.save(testParcoursMTI)
+  await AppDataSource.manager.save(testParcoursSRS)
+
+  const get = await request.get('/parcours').query({campus: 'Rennes', type: 'Master', cost: '15000'})
+
+  expect(get.statusCode).toBe(StatusCodes.OK)
+  expect(get.body.length).toBe(1)
+  expect(get.body[0].title).toBe(testParcoursSRS.title)
 })
