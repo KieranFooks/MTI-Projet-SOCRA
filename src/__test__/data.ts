e3e0d40d7e669dab3d836cc93334947100363214
Hugo Poujol <hugo.poujol@epita.fr>
2022-06-11 18:21:26 +0200

    refacto: Fix value redundancy in tests

---
+++ src/__test__/data.ts
@@ -0,0 +1,10 @@
+import { Module } from '../entity/Module'
+import { Parcours } from '../entity/Parcours'
+import { Subject } from '../entity/Subject'
+
+const testModuleMTI1 : Module = new Module('S1', [new Subject('Systèmes d\'information', 'Autour de la gestion de l\'information'), new Subject('ERP', 'Préparation Certification SAP')])
+const testModuleMTI2 : Module = new Module('S2', [new Subject('VueJS', 'Initiation au VueJS'), new Subject('SCRUM', 'Initiation au SCRUM')])
+const testModuleSRS1 : Module = new Module('S1', [new Subject('Forensic et reverse engineering', 'Initiation aux forensic et reverse engineering'), new Subject('Supervision de sécurité, ingénierie des SOC, détection opérationnelle', 'Initiation à la supervision de sécurité, ingénierie des SOC, détection opérationnelle')])
+const testModuleSRS2 : Module = new Module('S2', [new Subject('Stage en entreprise', 'Stage effectué en entreprise')])
+export const testParcoursMTI : Parcours = new Parcours('MTI', 'Paris', 24, 'Master', 20000, 80, new Date(), [testModuleMTI1, testModuleMTI2], 'Super Majeur')
+export const testParcoursSRS : Parcours = new Parcours('SRS', 'Rennes', 24, 'Master', 10000, 100, new Date(), [testModuleSRS1, testModuleSRS2], 'Majeur bof bof')
--- src/__test__/unit/controller/parcoursController.test.ts
+++ src/__test__/unit/controller/parcoursController.test.ts
@@ -1,10 +1,8 @@
 import { StatusCodes } from 'http-status-codes'
 import supertest from 'supertest'
 import app from '../../../app'
-import { Module } from '../../../entity/Module'
-import { Parcours } from '../../../entity/Parcours'
-import { Subject } from '../../../entity/Subject'
 import { parcoursService } from '../../../service'
+import { testParcoursMTI, testParcoursSRS } from '../../data'
 
 /**
  * User integration test
@@ -13,13 +11,6 @@ import { parcoursService } from '../../../service'
  */
 
 const request = supertest(app)
-
-const testModuleMTI1 : Module = new Module('S1', [new Subject('Systèmes d\'information', 'Autour de la gestion de l\'information'), new Subject('ERP', 'Préparation Certification SAP')])
-const testModuleMTI2 : Module = new Module('S2', [new Subject('VueJS', 'Initiation au VueJS'), new Subject('SCRUM', 'Initiation au SCRUM')])
-const testModuleSRS1 : Module = new Module('S1', [new Subject('Forensic et reverse engineering', 'Initiation aux forensic et reverse engineering'), new Subject('Supervision de sécurité, ingénierie des SOC, détection opérationnelle', 'Initiation à la supervision de sécurité, ingénierie des SOC, détection opérationnelle')])
-const testModuleSRS2 : Module = new Module('S2', [new Subject('Stage en entreprise', 'Stage effectué en entreprise')])
-const testParcoursMTI : Parcours = new Parcours('MTI', 'Paris', 24, 'Master', 20000, 80, new Date(), [testModuleMTI1, testModuleMTI2], 'Super Majeur')
-const testParcoursSRS : Parcours = new Parcours('SRS', 'Paris', 24, 'Master', 10000, 100, new Date(), [testModuleSRS1, testModuleSRS2], 'Majeur bof bof')
 
 test('should return 2 parcours when there is 2 parcours', async () => {
   parcoursService.getAll = jest.fn(async () => {
