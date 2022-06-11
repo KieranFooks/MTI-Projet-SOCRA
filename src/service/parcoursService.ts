497188320fbdf25b94251f2056f0a793fc19828b
Hugo Poujol <hugo.poujol@epita.fr>
2022-06-11 17:41:48 +0200

    fix: refacto parcoursService

--- src/service/parcoursService.ts
+++ src/service/parcoursService.ts
@@ -2,6 +2,6 @@ import { Parcours } from '../entity/Parcours'
 import { parcoursRepository } from '../repository'
 
 export async function getAll(campus: string | undefined = undefined, type : string | undefined = undefined, cost : number | undefined = undefined): Promise<Parcours[]> {
-  const parcours = parcoursRepository.getAll(campus, type, cost)
-  return (await parcours).reverse()
+  const parcours = await parcoursRepository.getAll(campus, type, cost)
+  return parcours.reverse()
 }
