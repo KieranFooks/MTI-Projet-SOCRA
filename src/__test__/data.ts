a068c582217de479e156df6aa3ec170b9a965e55
Hugo Poujol <hugo.poujol@epita.fr>
2022-06-11 18:29:36 +0200

    test: Add 3 users in data

--- src/__test__/data.ts
+++ src/__test__/data.ts
@@ -1,6 +1,7 @@
 import { Module } from '../entity/Module'
 import { Parcours } from '../entity/Parcours'
 import { Subject } from '../entity/Subject'
+import { User } from '../entity/User'
 
 const testModuleMTI1 : Module = new Module('S1', [new Subject('Systèmes d\'information', 'Autour de la gestion de l\'information'), new Subject('ERP', 'Préparation Certification SAP')])
 const testModuleMTI2 : Module = new Module('S2', [new Subject('VueJS', 'Initiation au VueJS'), new Subject('SCRUM', 'Initiation au SCRUM')])
@@ -8,3 +9,7 @@ const testModuleSRS1 : Module = new Module('S1', [new Subject('Forensic et rever
 const testModuleSRS2 : Module = new Module('S2', [new Subject('Stage en entreprise', 'Stage effectué en entreprise')])
 export const testParcoursMTI : Parcours = new Parcours('MTI', 'Paris', 24, 'Master', 20000, 80, new Date(), [testModuleMTI1, testModuleMTI2], 'Super Majeur')
 export const testParcoursSRS : Parcours = new Parcours('SRS', 'Rennes', 24, 'Master', 10000, 100, new Date(), [testModuleSRS1, testModuleSRS2], 'Majeur bof bof')
+
+export const user1 : User = new User('user1@gmail.com', 'password')
+export const user2 : User = new User('user2@hotmail.com', '123456')
+export const user3 : User = new User('user3@orange.fr', 'pwd')
